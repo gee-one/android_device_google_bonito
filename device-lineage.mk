eb8069f1cff0d45cdb72ba46af665c031d679c95
gee-one <admin@brownbagdinner.com>
2019-07-23 20:48:20 -0500

    Revert "b4s4: Specify that this is a Fi device"
    This reverts commit b2356b2dd617ce1e00fdae130cb310c0abc5e61b.
    
        we don't have the google fi vendor repo, so let's not set this flag
    
    Change-Id: If400f809f014e293e55d7ecdb50724a778be501d

--- device-lineage.mk
+++ device-lineage.mk
@@ -10,7 +10,5 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
-WITH_GMS_FI := true
-
 PRODUCT_COPY_FILES += \
     device/google/bonito/permissions/privapp-permissions-aosp-extended.xml:system/etc/permissions/privapp-permissions-aosp-extended.xml
