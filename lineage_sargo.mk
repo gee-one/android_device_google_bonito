b55d5fd0bf072e59effe2acb6ed470bece9fc313
gee-one <admin@brownbagdinner.com>
2019-08-06 13:11:02 -0500

    b4s4: sargo only update fingerprint for 201908
    Change-Id: Ia26b6ffe3ecbe9eed14b7de2598dc20dafec96d8

--- lineage_sargo.mk
+++ lineage_sargo.mk
@@ -19,9 +19,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sargo \
-    PRIVATE_BUILD_DESC="sargo-user 9 PQ3B.190705.003 5622519 release-keys"
+    PRIVATE_BUILD_DESC="sargo-user 9 PQ3B.190801.002 5674421 release-keys"
 
-BUILD_FINGERPRINT := google/sargo/sargo:9/PQ3B.190705.003/5622519:user/release-keys
+BUILD_FINGERPRINT := google/sargo/sargo:9/PQ3B.190801.002/5674421:user/release-keys
 
 # Include sargo specific blobs
 $(call inherit-product-if-exists, vendor/google/sargo/sargo-vendor.mk)
